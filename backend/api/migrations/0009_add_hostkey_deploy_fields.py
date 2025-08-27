8a015613f34e5548c469daa7841aea42585e9e34
Sugam Dahal <sugamdahalos@gmail.com>
2025-08-27 11:43:13 +0545

    Fix migration dependency name for HostKey deployment fields

--- backend/api/migrations/0009_add_hostkey_deploy_fields.py
+++ backend/api/migrations/0009_add_hostkey_deploy_fields.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0008_add_hostkey'),
+    ('api', '0008_hostkey'),
     ]
 
     operations = [
